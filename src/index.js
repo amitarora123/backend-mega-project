2eb54897bd65cf20d473b0f442c7b20ad6462472
Amit <baghlaamit06@gmail.com>
2025-01-27 22:11:01 +0530

    add the asyncHandler and ApiErr utility files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env", // Adjust the path as per your directory structure
+  path: "./env", 
 });
 
 connectDB()
@@ -15,24 +15,3 @@ connectDB()
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
